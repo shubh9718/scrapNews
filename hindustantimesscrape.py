b5f36af4b91697a2e4098b3ea8e173d518e3389b
shubh9718 <shubham.srivastava1719@gmail.com>
2018-11-23 00:57:04 +0530

    Second commit

--- hindustantimesscrape.py
+++ hindustantimesscrape.py
@@ -1,12 +1,31 @@
 import requests 
 import csv
 import pandas as pd
-
 from bs4 import BeautifulSoup 
 
-def htnews(): 
-    url1='https://www.hindustantimes.com/latest-news'
-    # url2='https://www.hindustantimes.com/top-news'
-    
-    web_response = requests.get(url1) 
-    
+def hindustannews():
+    #saving the url
+    url='https://www.hindustantimes.com/latest-news'
+
+    #getting the url through requests module
+    webResponse = requests.get(url) 
+
+    #check if the server successfully answered the request
+    #200 is the HTTP status code for a successful response
+    if webResponse.status_code==200:
+        
+        #extract data from everything(text) on the page
+        soup = BeautifulSoup(webResponse.text,'html.parser')
+
+        #find elements with <ul> tag and classs="latest-news-bx"
+        nlist = soup.find("ul",{"class":"latest-news-bx"}) 
+
+        #create a csv file if not already exists named "News.csv"
+        with open("News.csv",'w',encoding="utf-8") as nfle:
+            writer = csv.writer(nfle, dialect='excel')
+
+            #iterate data over the rows with headers 'News and 'Link'
+            writer.writerow(['News','Link'])
+
+            
+hindustannews()
